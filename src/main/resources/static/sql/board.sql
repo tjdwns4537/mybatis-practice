2d7763448d0b737f8baa790a8c18199df76499e6
박성준 <63576379+tjdwns4537@users.noreply.github.com>
2023-08-21 14:12:28 +0900

    Revert "Feat#5"

--- src/main/resources/static/sql/board.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE boards(
-                       board_id INT NOT NULL AUTO_INCREMENT,
-                       board_title VARCHAR(256),
-                       board_content LONGTEXT,
-                       create_at TIMESTAMP,
-                       modify_at TIMESTAMP,
-                       PRIMARY KEY(board_id)
-)
-
--- src/main/resources/static/sql/user.sql
+++ src/main/resources/static/sql/user.sql
@@ -1,37 +1,11 @@
 /** CREATE user **/
 
-CREATE TABLE users(
+CREATE TABLE user(
                      user_id INT NOT NULL AUTO_INCREMENT,
                      user_name VARCHAR(64),
                      user_email VARCHAR(128),
                      user_password VARCHAR(128),
                      create_at TIMESTAMP,
-                     modify_At TIMESTAMP,
+                     modified_At TIMESTAMP,
                      PRIMARY KEY(user_id)
 )
-
-/** INSERT user **/
-
-INSERT INTO users(
-    user_name, user_email, user_password, create_at, modified_at
-    ) VALUES (
-    'test','test@com','test123','2023-08-10','2023-08-10'
-    );
-
-
-/** EDIT column **/
-
-ALTER TABLE users MODIFY COLUMN user_name varchar(32) not null;
-
-ALTER TABLE users MODIFY COLUMN user_email varchar(128) not null;
-
-ALTER TABLE users MODIFY COLUMN user_password varchar(64) not null;
-
-ALTER TABLE users MODIFY COLUMN create_at varchar(64) not null;
-
-ALTER TABLE users MODIFY COLUMN modify_At varchar(64) null;
-
-ALTER TABLE users CHANGE modify_At modify_at TIMESTAMP;
-
-
-/****/
